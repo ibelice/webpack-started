7a13e3640b69aa848ec2018da76b59acf5fc7eaf
Ibelice <ipulvett@gmail.com>
2021-09-27 10:53:01 -0400

    primer commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpack    = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin     = require("copy-webpack-plugin");
+
+module.exports = {
+
+    mode: 'development',
+    output: {//borra el dist y lo realiza de nuevo sin necesidad de borrarlo manualmente
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test:/\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    optimization: {},
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',//cambia el titulo del html
+            filename:'index.html',//cambia el nombre del index de dist
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin ({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ]
+}
